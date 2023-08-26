0c65b507cbd50497dd15a880f4e8728fc8b19d6f
unknown <mahekmj123@gmail.com>
2023-08-26 06:56:53 +0530

    navbar fixed

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -12,7 +12,7 @@ import Footer from "../../components/Footer";
 const LandingPage = () => {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <div>
         <div className="flex justify-center">
           <div>

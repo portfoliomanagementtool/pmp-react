9e692f100c85eb547e6375bdb3db1aa25817a644
unknown <mahekmj123@gmail.com>
2023-10-04 18:50:42 +0530

    history transaction card created

---
+++ src/pages/Dashboard/components/HistoryRow.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const HistoryRow = () => {
+  return (
+    <>
+      <div className="flex w-full font-Poppins">
+        <h1>Assets</h1>
+        <h2>Amount</h2>
+        <h2>Date</h2>
+      </div>
+      <hr />
+    </>
+  );
+};
+
+export default HistoryRow;

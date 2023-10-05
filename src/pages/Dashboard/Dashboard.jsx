e1c1fcd209369e6e010fd3f963c9be2e8d4a1bb4
unknown <mahekmj123@gmail.com>
2023-10-05 21:36:49 +0530

    navbar fixed

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -13,10 +13,9 @@ import D3_Chart from "./components/D3_Chart";
 import SipCalculator from "./components/SipCalculator";
 import HighCharts_Chart from "./components/HighCharts_Chart";
 import ProfitLossCard from "./components/ProfitLossCard";
-
+import RecentlyVisited from "./components/RecentlyVisited";
 import TransactionHistory from "./components/TransactionHistory";
 import Navbar from "../../components/Navbar";
-
 
 Chart.register(CategoryScale);
 
@@ -64,7 +63,6 @@ const Dashboard = () => {
   ];
 
   const ProfitData = [
-
     { title: "Market Value", Amount: "123456", percentage: "-90.56(-0.47%)" },
     { title: "Day P/L", Amount: "+2456", percentage: "2456(+0.27%)" },
     {
@@ -74,12 +72,11 @@ const Dashboard = () => {
     },
   ];
   return (
-    
     <div className="flex font-poppins bg-[#F3F4F9]">
       <Sidebar className="left-0 h-full" />
       {/* <UserButton /> */}
       <div className="w-full flex flex-col">
-        <Navbar/>
+        <Navbar />
         <div className="grid grid-cols-3  ">
           {/* {ProfitData?.map(obj=>{
         
@@ -90,17 +87,13 @@ const Dashboard = () => {
             return <ProfitLossCard key={obj?.title} {...obj} />;
           })}
         </div>
-        <TransactionHistory/>
+        <div className="flex">
+          <TransactionHistory />
+          <RecentlyVisited />
+        </div>
+      </div>
 
-      </div>
-      <div className="grid grid-cols-2">
-          <RecentlyVisited/>
-          <RecentlyVisited/>
-      </div>
-      <div>
-
-      </div>
-      </div>
+      <div></div>
     </div>
   );
 };

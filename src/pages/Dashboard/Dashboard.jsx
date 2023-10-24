be2df6398908084e664418a5bec74fcbf05b1077
unknown <mahekmj123@gmail.com>
2023-10-24 12:41:42 +0530

    portfolio table functionality added

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -2,9 +2,6 @@ import React, { useState } from "react";
 import { UserButton, useUser } from "@clerk/clerk-react";
 import Sidebar from "../../components/Sidebar";
 import PerformerCard from "./components/PerformerCard";
-// import "slick-carousel/slick/slick.css";
-// import "slick-carousel/slick/slick-theme.css";
-// import Slider from "react-slick";
 import Chart from "chart.js/auto";
 import { CategoryScale } from "chart.js";
 import LineChart from "./components/LineChart";
@@ -13,12 +10,13 @@ import D3_Chart from "./components/D3_Chart";
 import SipCalculator from "./components/SipCalculator";
 import HighCharts_Chart from "./components/HighCharts_Chart";
 import ProfitLossCard from "./components/ProfitLossCard";
-
 import TransactionHistory from "./components/TransactionHistory";
 import RecentlyVisited from "./components/RecentlyVisited";
 import Navbar from "../../components/Navbar";
 import Graphs from "./components/CryptoGraph";
 import MonthlyInvestment from "./components/MonthlyInvestment";
+import SellBuyTable from "../Portfolio/components/SellBuyTable";
+import Modal from "../Portfolio/components/Modal";
 
 Chart.register(CategoryScale);
 
@@ -26,6 +24,170 @@ const Dashboard = () => {
   const { isLoaded, isSignedIn, user } = useUser();
   const [interval, setInterval] = useState("day");
   const [chartData, setChartData] = useState(getChartData(interval));
+
+  const [modalOpen, setModalOpen] = useState(false);
+  const [rows, setRows] = useState([
+    {
+      category: "Technology",
+      ticker: "AAPL",
+      price: 150.5,
+      avgBasis: 140.25,
+      qty: 100,
+      marketValue: 15050.0,
+      costBasis: 14025.0,
+      profitLoss: 1025.0,
+      percentPL: 7.32,
+      portfolioPercent: 12.5,
+      categoryPercent: 10.2,
+      // status: "live",
+    },
+    {
+      category: "Finance",
+      ticker: "GS",
+      price: 380.75,
+      avgBasis: 375.5,
+      qty: 50,
+      marketValue: 19037.5,
+      costBasis: 18775.0,
+      profitLoss: 262.5,
+      percentPL: 1.4,
+      portfolioPercent: 8.3,
+      categoryPercent: 9.1,
+      // status: "draft",
+    },
+    {
+      category: "Healthcare",
+      ticker: "PFE",
+      price: 45.2,
+      avgBasis: 47.1,
+      qty: 200,
+      marketValue: 9040.0,
+      costBasis: 9420.0,
+      profitLoss: -380.0,
+      percentPL: -4.03,
+      portfolioPercent: 9.7,
+      categoryPercent: 11.8,
+      // status: "error",
+    },
+    {
+      category: "Energy",
+      ticker: "XOM",
+      price: 62.85,
+      avgBasis: 64.2,
+      qty: 150,
+      marketValue: 9427.5,
+      costBasis: 9630.0,
+      profitLoss: -202.5,
+      percentPL: -2.1,
+      portfolioPercent: 7.1,
+      categoryPercent: 8.5,
+      // status: "live",
+    },
+    {
+      category: "Consumer Goods",
+      ticker: "TSLA",
+      price: 750.2,
+      avgBasis: 740.0,
+      qty: 60,
+      marketValue: 45012.0,
+      costBasis: 44400.0,
+      profitLoss: 612.0,
+      percentPL: 1.38,
+      portfolioPercent: 10.2,
+      categoryPercent: 10.2,
+      // status: "draft",
+    },
+    {
+      category: "Transportation",
+      ticker: "DAL",
+      price: 42.65,
+      avgBasis: 40.75,
+      qty: 130,
+      marketValue: 5544.5,
+      costBasis: 5307.5,
+      profitLoss: 237.0,
+      percentPL: 4.47,
+      portfolioPercent: 6.2,
+      categoryPercent: 7.3,
+      // status: "live",
+    },
+    {
+      category: "Technology",
+      ticker: "GOOGL",
+      price: 2600.75,
+      avgBasis: 2650.25,
+      qty: 30,
+      marketValue: 78022.5,
+      costBasis: 79507.5,
+      profitLoss: -1485.0,
+      percentPL: -1.87,
+      portfolioPercent: 18.3,
+      categoryPercent: 10.1,
+      // status: "error",
+    },
+    {
+      category: "Finance",
+      ticker: "JPM",
+      price: 160.4,
+      avgBasis: 156.75,
+      qty: 80,
+      marketValue: 12832.0,
+      costBasis: 12540.0,
+      profitLoss: 292.0,
+      percentPL: 2.32,
+      portfolioPercent: 5.5,
+      categoryPercent: 9.9,
+      // status: "live",
+    },
+    {
+      category: "Healthcare",
+      ticker: "MRNA",
+      price: 300.6,
+      avgBasis: 290.75,
+      qty: 40,
+      marketValue: 12024.0,
+      costBasis: 11630.0,
+      profitLoss: 394.0,
+      percentPL: 3.39,
+      portfolioPercent: 4.7,
+      categoryPercent: 10.9,
+      // status: "draft",
+    },
+    {
+      category: "Energy",
+      ticker: "CVX",
+      price: 104.5,
+      avgBasis: 105.75,
+      qty: 100,
+      marketValue: 10450.0,
+      costBasis: 10575.0,
+      profitLoss: -125.0,
+      percentPL: -1.18,
+      portfolioPercent: 3.8,
+      categoryPercent: 7.8,
+      // status: "error",
+    },
+  ]);
+  const [rowToEdit, setRowToEdit] = useState(null);
+  const handleDeleteRow = (targetIndex) => {
+    setRows(rows.filter((_, idx) => idx !== targetIndex));
+  };
+  const handleEditRow = (idx) => {
+    setRowToEdit(idx);
+    setModalOpen(true);
+  };
+
+  const handleSubmit = (newRow) => {
+    rowToEdit === null
+      ? setRows([...rows, newRow])
+      : setRows(
+          rows.map((currRow, idx) => {
+            if (idx !== rowToEdit) return currRow;
+
+            return newRow;
+          })
+        );
+  };
 
   const handleIntervalChange = (selectedInterval) => {
     setInterval(selectedInterval);
@@ -97,6 +259,33 @@ const Dashboard = () => {
           <div className="">
             <MonthlyInvestment />
           </div>
+        </div>
+        <div className="flex flex-col items-center justify-center bg-gray-100">
+          <SellBuyTable
+            rows={rows}
+            deleteRow={handleDeleteRow}
+            editRow={handleEditRow}
+          />
+          <button
+            onClick={() => {
+              setModalOpen(true);
+              setRowToEdit(null);
+            }}
+            className="btn mt-4 bg-blue-600 text-white px-4 py-2 rounded cursor-pointer shadow-md"
+          >
+            Add
+          </button>
+
+          {modalOpen && (
+            <Modal
+              closeModal={() => {
+                setModalOpen(false);
+                setRowToEdit(null);
+              }}
+              onSubmit={handleSubmit}
+              defaultValue={rowToEdit !== null && rows[rowToEdit] } 
+            />
+          )}
         </div>
       </div>
     </div>

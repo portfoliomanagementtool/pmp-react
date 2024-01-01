419252baacb522ecea98506ca78b48c8ff917688
Ronnit Mirgh <ronnit.mirgh@gmail.com>
2024-01-01 19:53:50 +0530

    notification and user dropdown

--- src/components/MenuCollapsed/MenuItemCollapsed.jsx
+++ src/components/MenuCollapsed/MenuItemCollapsed.jsx
@@ -7,7 +7,7 @@ import MenuSubItemCollapsed from './MenuSubItemCollapsed';
 
 const MenuItemCollapsed = ({ item }) => {
   const dispatch = useDispatch();
-  const { mode, active} = useSelector((state) => state.config);
+  const { active} = useSelector((state) => state.config);
   const [show, setShow] = useState(false);
 
   const handleMenuItemClick = () => {
@@ -26,13 +26,14 @@ const MenuItemCollapsed = ({ item }) => {
             </Link>
           </div>
           <Tooltip
-          anchorSelect={"#" + item.id} 
-          wrapper="span"
-          className="tooltip !bg-gray-800 dark:!bg-slate-100"
-          content={item.name} 
-          place="right"
-          variant={mode === "light" ? "dark" : "light"}
-        />
+            anchorSelect={"#" + item.id} 
+            wrapper="span"
+            // className="tooltip !bg-gray-800 dark:!bg-slate-100"
+            className="tooltip !bg-gray-800 dark:!bg-black"
+            content={item.name} 
+            place="right"
+            // variant={mode === "light" ? "dark" : "light"}
+          />
       </>
       ) : (
         <div 

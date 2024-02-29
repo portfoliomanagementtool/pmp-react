b4a236828435d34b91e13cb94c72dbd29042cdcb
Mahek Jain <mahekmj123@gmail.com>
2024-02-29 12:44:24 +0530

    portfolio div fixed

--- src/pages/Portfolio/components/Modals/Modal.jsx
+++ src/pages/Portfolio/components/Modals/Modal.jsx
@@ -6,9 +6,11 @@ import { FiSearch } from "react-icons/fi";
 import BuySellModal from "./BuySellModal";
 import MoreDetailsModal from "./MoreDetailsModal";
 import { useDispatch } from "react-redux";
-import { saveWatchlistAsset } from "../../../../state/slices/watchlistSlice";
+// import { saveWatchlistAsset } from "../../../../state/slices/watchlistSlice";
 
-const Modal = ({ onSubmit, closeModal, defaultValue, position, idx }) => {
+const Modal = ({ onSubmit, closeModal, defaultValue }) => {
+  const [isModalOpen, setModalOpen] = useState(false);
+
   console.log(defaultValue);
   const dispatch = useDispatch();
 
@@ -41,120 +43,104 @@ const Modal = ({ onSubmit, closeModal, defaultValue, position, idx }) => {
     setSellBuyModalOpen(true);
   };
 
-  const handleClose = (e) => {
+  const handleClose = () => {
+    console.log("hi");
+    setModalOpen(false);
     closeModal();
   };
 
   const watch = ({ ticker, quantity, price, category }) => {
-    dispatch(saveWatchlistAsset({ ticker, quantity, price, category }));
+    // dispatch(saveWatchlistAsset({ ticker, quantity, price, category }));
     navigate("/app/watchlist");
   };
 
   return (
-    <div className="absolute">
-      <div className=" bg-white w-72 p-6 rounded-md z-10">
-        <div className="flex justify-between items-center mb-4">
-          <h3 className="text-xl font-semibold">Asset Details</h3>
-          <button
-            className="text-gray-600 font-bold hover:text-gray-800"
-            onClick={handleClose}
-          >
-            <AiOutlineClose />
-          </button>
-        </div>
-        <div className="flex mx-auto justify-center items-center mt-4">
-          <button
-            className={`${
-              activeTab === "buy" ? "bg-red-500" : "bg-gray-300"
-            } text-white px-4 py-2 rounded-l cursor-pointer`}
-            onClick={() => handleTabClick("buy")}
-          >
-            Buy
-          </button>
-          <button
-            className={`${
-              activeTab === "sell" ? "bg-green-500" : "bg-gray-300"
-            } text-white px-4 py-2 rounded-r cursor-pointer`}
-            onClick={() => handleTabClick("sell")}
-          >
-            Sell
-          </button>
-        </div>
-        <div>
-          {sellBuyModalOpen && (
-            <BuySellModal
-              onSubmit={handleClose}
-              closeModal={closeBuySellModal}
-              initialChecked={activeTab === "sell"}
-              defaultValue={defaultValue}
-            />
-          )}
-        </div>
-        <div>
-          <div className="w-full mx-auto">
-            <div className="w-full flex justify-start bg-white border-b hover:bg-slate-50 dark:hover:opacity-80 dark:hover:bg-slate-700 dark:bg-gray-800 dark:border-gray-700">
-              <div className="py-4 pr-6 pl-2 flex text-xl font-bold text-gray-900 dark:text-white">
-                <MdOutlineReadMore />
-              </div>
-              <span
-                className="py-4 justify-start text-gray-900 dark:text-white cursor-pointer select-none font-semibold hover:text-gray-600"
-                onClick={() => setMoreDetailsModalOpen(true)}
-              >
-                More Details
-              </span>
-            </div>
-            <div className="w-full flex justify-start bg-white border-b hover:bg-slate-50 dark:hover:opacity-80 dark:hover:bg-slate-700 dark:bg-gray-800 dark:border-gray-700">
-              <div className="py-4 pr-6 pl-2 flex text-xl font-bold text-gray-900 dark:text-white">
-                <FiSearch />
-              </div>
-              <span
-                className="py-4 justify-start text-gray-900 dark:text-white cursor-pointer select-none font-semibold hover:text-gray-600"
-                onClick={() => navigate("/app/asset/view")}
-              >
-                View Asset
-              </span>
-            </div>
-            <div className="w-full flex justify-start bg-white border-b hover:bg-slate-50 dark:hover:opacity-80 dark:hover:bg-slate-700 dark:bg-gray-800 dark:border-gray-700">
-              <div className="py-4 pr-6 pl-2 flex text-xl font-bold text-gray-900 dark:text-white">
-                <MdChecklistRtl />
-              </div>
-              <span
-                className="py-4 justify-start text-gray-900 dark:text-white cursor-pointer select-none font-semibold hover:text-gray-600"
-                onClick={() => watch(defaultValue)}
-              >
-                Watchlist
-              </span>
-            </div>
+      <div className="absolute right-[17%] ">
+        <div className=" dark:bg-gray-800 bg-white border w-72 p-6 -mt-7 rounded-md z-10">
+          <div className="flex justify-between items-center mb-4">
+            <h3 className="text-xl font-semibold">Asset Details</h3>
+            <button
+              className="text-gray-600 dark:text-gray-400 font-bold hover:text-gray-800 dark:hover:text-gray-200"
+              onClick={handleClose}
+            >
+              <AiOutlineClose />
+            </button>
           </div>
-          {moreDetailsModalOpen && (
-            <MoreDetailsModal
-              closeModal={() => setMoreDetailsModalOpen(false)}
-              defaultValue={defaultValue}
-            />
-          )}
+          <div className="flex mx-auto justify-center items-center mt-4">
+            <button
+              className={`${
+                activeTab === "buy" ? "bg-red-500" : "bg-gray-300"
+              } text-white px-4 py-2 rounded-l cursor-pointer`}
+              onClick={() => handleTabClick("buy")}
+            >
+              Buy
+            </button>
+            <button
+              className={`${
+                activeTab === "sell" ? "bg-green-500" : "bg-gray-300"
+              } text-white px-4 py-2 rounded-r cursor-pointer`}
+              onClick={() => handleTabClick("sell")}
+            >
+              Sell
+            </button>
+          </div>
+          <div>
+            {sellBuyModalOpen && (
+              <BuySellModal
+                onSubmit={handleClose}
+                closeModal={closeBuySellModal}
+                initialChecked={activeTab === "sell"}
+                defaultValue={defaultValue}
+              />
+            )}
+          </div>
+          <div>
+            <div className="w-full mx-auto">
+              <div className="w-full flex justify-start bg-white border-b hover:bg-slate-50 dark:hover:opacity-80 dark:hover:bg-slate-700 dark:bg-gray-800 dark:border-gray-700">
+                <div className="py-4 pr-6 pl-2 flex text-xl font-bold text-gray-900 dark:text-white">
+                  <MdOutlineReadMore />
+                </div>
+                <span
+                  className="py-4 justify-start text-gray-900 dark:text-white cursor-pointer select-none font-semibold hover:text-gray-600"
+                  onClick={() => setMoreDetailsModalOpen(true)}
+                >
+                  More Details
+                </span>
+              </div>
+              <div className="w-full flex justify-start bg-white border-b hover:bg-slate-50 dark:hover:opacity-80 dark:hover:bg-slate-700 dark:bg-gray-800 dark:border-gray-700">
+                <div className="py-4 pr-6 pl-2 flex text-xl font-bold text-gray-900 dark:text-white">
+                  <FiSearch />
+                </div>
+                <span
+                  className="py-4 justify-start text-gray-900 dark:text-white cursor-pointer select-none font-semibold hover:text-gray-600"
+                  onClick={() => navigate("/app/asset/view")}
+                >
+                  View Asset
+                </span>
+              </div>
+              <div className="w-full flex justify-start bg-white border-b hover:bg-slate-50 dark:hover:opacity-80 dark:hover:bg-slate-700 dark:bg-gray-800 dark:border-gray-700">
+                <div className="py-4 pr-6 pl-2 flex text-xl font-bold text-gray-900 dark:text-white">
+                  <MdChecklistRtl />
+                </div>
+                <span
+                  className="py-4 justify-start text-gray-900 dark:text-white cursor-pointer select-none font-semibold hover:text-gray-600"
+                  onClick={() => watch(defaultValue)}
+                >
+                  Watchlist
+                </span>
+              </div>
+            </div>
+            {moreDetailsModalOpen && (
+              <MoreDetailsModal
+                closeModal={() => setMoreDetailsModalOpen(false)}
+                defaultValue={defaultValue}
+              />
+            )}
+          </div>
         </div>
       </div>
-    </div>
+    
   );
 };
 
 export default Modal;
-
-// import React from 'react'
-
-// const Modal = () => {
-//   return (
-//     <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
-//     <div class="py-1" role="none">
-//       <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Account settings</a>
-//       <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Support</a>
-//       <a href="#" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">License</a>
-//       <form method="POST" action="#" role="none">
-//         <button type="submit" class="text-gray-700 block w-full px-4 py-2 text-left text-sm" role="menuitem" tabindex="-1" id="menu-item-3">Sign out</button>
-//       </form>
-//     </div>
-//   </div>
-//   )
-// }
-
-// export default Modal
--- src/pages/Portfolio/components/SellBuyTable.jsx
+++ src/pages/Portfolio/components/SellBuyTable.jsx
@@ -36,36 +36,21 @@ const SellBuyTable = ({ rows, deleteRow }) => {
     }
   };
 
-  const [isModalOpen, setModalOpen] = useState(false);
+  const [isModalOpen, setModalOpen] = useState(Array(rows.length).fill(false));
+
   const [selectedRowData, setSelectedRowData] = useState(null);
-  const [clickPosition, setClickPosition] = useState({ top: 0, left: 0 });
-  const [selectedRowID, setSelectedRowID] = useState(null);
-  const [isRowModalOpen, setRowModalOpen] = useState(false);
-  const [rowModalData, setRowModalData] = useState(null);
 
-  // const handleRowMouseEnter = (e, rowData) => {
-  //   console.log("Mouse Enter Triggered");
-  //   const rect = e.target.getBoundingClientRect();
-  //   setRowModalData(rowData);
-  //   setRowModalOpen(true);
-  //   setClickPosition({
-  //     top: rect.top + window.scrollY,
-  //     left: rect.left + window.scrollX,
-  //   });
-  // };
-
-  // const handleRowMouseLeave = () => {
-  //   setRowModalOpen(false);
-  //   setRowModalData(null);
-  // };
-
-  const openModal = (e, rowData, idx) => {
-    setModalOpen(true);
-    setSelectedRowData(rowData);
+  const openModal = (idx, row) => {
+    const updatedModalOpenStates = [...isModalOpen];
+    updatedModalOpenStates[idx] = true;
+    setModalOpen(updatedModalOpenStates);
+    setSelectedRowData(row);
   };
 
-  const closeModal = () => {
-    setModalOpen(false);
+  const closeModal = (idx) => {
+    const updatedModalOpenStates = [...isModalOpen];
+    updatedModalOpenStates[idx] = false;
+    setModalOpen(updatedModalOpenStates);
     setSelectedRowData(null);
   };
 
@@ -105,21 +90,6 @@ const SellBuyTable = ({ rows, deleteRow }) => {
                 </span>
               </button>
 
-              {/* <a
-                download=""
-                className="block lg:inline-block md:mb-0 mb-4"
-                href="/data/product-list.csv"
-                target="_blank"
-              >
-                <button className="button bg-white border border-gray-300 dark:bg-gray-700 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 active:bg-gray-100 dark:active:bg-gray-500 dark:active:border-gray-500 text-gray-600 dark:text-gray-100 radius-round h-9 px-3 py-2 text-sm w-full">
-                  <span className="flex items-center justify-center">
-                    <span className="text-lg">
-                      <RiDownloadLine />
-                    </span>
-                    <span className="ml-1 mr-1">Export</span>
-                  </span>
-                </button>
-              </a> */}
               <a
                 className="block lg:inline-block md:mb-0 mb-4"
                 href="/app/funds/ticker-new"
@@ -230,34 +200,24 @@ const SellBuyTable = ({ rows, deleteRow }) => {
                             <div className="flex  justify-end text-lg">
                               <span
                                 className="cursor-pointer p-2  hover:text-indigo-600"
-                                onClick={() => handleThreeDotsClick(idx)}
+                                onClick={() => openModal(idx, row)}
                               >
                                 <BsThreeDotsVertical />
                               </span>
                             </div>
                           </td>
                         </tr>
-                        {detail === idx && (
-                          <div>
-                            <td colSpan="6">
-                              <div className="justify-end bg-green-500 p-2 text-white">
-                                Hello
-                              </div>
-                            </td>
-                          </div>
+                        {isModalOpen[idx] && (
+                          <Modal
+                            onSubmit={closeModal}
+                            closeModal={() => closeModal(idx)}
+                            defaultValue={selectedRowData}
+                          />
                         )}
                       </React.Fragment>
                     );
                   })}
                 </tbody>
-                {/* {isModalOpen && (
-                  <Modal
-                    onSubmit={closeModal}
-                    closeModal={closeModal}
-                    defaultValue={selectedRowData}
-                    position={clickPosition}
-                  />
-                )} */}
               </table>
             </div>
           </div>
